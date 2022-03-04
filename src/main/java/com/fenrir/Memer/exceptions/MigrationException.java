7fda4dae47ef5f9b6e1f2e0ca579a85e84e5d5cd
Fenrir7734 <hitex1999@gmail.com>
2022-03-04 14:07:00 +0100

    Constructors order changed

--- src/main/java/com/fenrir/Memer/exceptions/MigrationException.java
+++ src/main/java/com/fenrir/Memer/exceptions/MigrationException.java
@@ -1,11 +1,11 @@
 package com.fenrir.Memer.exceptions;
 
 public class MigrationException extends Exception {
-    public MigrationException(String message, Throwable cause) {
-        super(message, cause);
+    public MigrationException(String message) {
+        super(message);
     }
 
-    public MigrationException(String message) {
-        this(message, null);
+    public MigrationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
