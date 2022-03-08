6b027ac2b5116b72dcbc23a101b08067c9c97873
Fenrir7734 <hitex1999@gmail.com>
2022-03-08 11:16:39 +0100

    Adding and removing subreddits and Imgur tags

--- src/main/java/com/fenrir/Memer/command/commands/Settings.java
+++ src/main/java/com/fenrir/Memer/command/commands/Settings.java
@@ -1,11 +1,14 @@
 package com.fenrir.Memer.command.commands;
 
 import com.fenrir.Memer.Memer;
+import com.fenrir.Memer.api.Reddit;
 import com.fenrir.Memer.command.CommandEvent;
 import com.fenrir.Memer.database.DatabaseService;
 import com.fenrir.Memer.database.entities.GuildDB;
+import com.fenrir.Memer.database.entities.GuildResourceEntity;
 import com.fenrir.Memer.database.entities.ImgurTagDB;
 import com.fenrir.Memer.database.entities.SubredditDB;
+import com.fenrir.Memer.database.managers.GuildResourceEntityManager;
 import com.fenrir.Memer.exceptions.BotRuntimeException;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -15,8 +18,10 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 public class Settings implements Command {
@@ -24,10 +29,12 @@ public class Settings implements Command {
 
     private final DatabaseService databaseService;
     private final List<String> defaultImgurTags;
+    private final Reddit redditMediaProvider;
 
     public Settings(Memer memer) {
         this.databaseService = memer.getDatabaseService();
         this.defaultImgurTags = memer.getSettings().getImgurTags();
+        this.redditMediaProvider = memer.getRedditMediaProvider();
     }
 
     @Override
@@ -63,8 +70,19 @@ public class Settings implements Command {
             } else {
                 channel.sendMessage("Sorry, I don't recognize this action.").queue();
             }
+        } else if (args.length == 3) {
+            String action = args[0];
+            if (action.equals("reddit")) {
+                customizeReddit(event, args[1], args[2]);
+            } else if (action.equals("imgur")) {
+                customizeImgur(event, args[1], args[2]);
+            } else {
+                channel.sendMessage("Sorry, I don't recognize this site.").queue();
+            }
         } else {
-            channel.sendMessage("Sorry, I could not resolve this command.").queue();
+            channel.sendMessage(
+                    "Sorry, I could not resolve this command. To check command usage type `<prefix>help settings`."
+            ).queue();
         }
     }
 
@@ -126,7 +144,7 @@ public class Settings implements Command {
         String enabledTags = buildColumnLayout(imgurTags, 2, 26, "+ ");
         String disabledTags = buildColumnLayout(differences, 2, 26, "- ");
 
-        return String.format("**Imgur**\n```%s%s```\n", enabledTags, disabledTags);
+        return String.format("**Imgur**\n```%s\n%s```\n", enabledTags, disabledTags);
     }
 
     private String buildColumnLayout(List<String> list, int colNum, int padding, String sign) {
@@ -174,9 +192,10 @@ public class Settings implements Command {
 
         Optional<Boolean> isNSFWOptional = stringToBoolean(value);
         if (isNSFWOptional.isEmpty()) {
-            channel.sendMessage(
-                    "Invalid value, you should write **enable** or **disable** to allow or forbid sending NSFW memes"
-            ).queue();
+            String message = String.format(
+                    "`%s` is incorrect value, type `<prefix>help settings` to see usage of this command.", value
+            );
+            channel.sendMessage(message).queue();
             return;
         }
 
@@ -204,6 +223,143 @@ public class Settings implements Command {
             return Optional.of(false);
         } else {
             return Optional.empty();
+        }
+    }
+
+    private void customizeReddit(CommandEvent event, String action, String value) {
+        if (action.equals("add")) {
+            addSubreddit(event, value);
+        } else if (action.equals("remove")) {
+            removeSubreddit(event, value);
+        } else {
+            String message = String.format(
+                    "`%s` is incorrect action, type `<prefix>help settings` to see usage of this command.", action
+            );
+            event.getChannel().sendMessage(message).queue();
+        }
+    }
+
+    private void addSubreddit(CommandEvent event, String value) {
+        TextChannel channel = event.getChannel();
+        try {
+            value = value.toLowerCase(Locale.ROOT).trim();
+
+            if (!redditMediaProvider.ping(value)) {
+                channel.sendMessage(
+                        "Sorry, I could not connect with given subreddit. Check if you provided correct name."
+                ).queue();
+                return;
+            }
+
+            long guildId = event.getGuildId();
+            GuildResourceEntityManager<SubredditDB> entityManager = databaseService.getSubredditService().get(guildId);
+            SubredditDB entity = new SubredditDB(value, guildId);
+            addToDatabase(entityManager, entity, channel, "subreddit");
+
+        } catch (IOException | InterruptedException e) {
+            String message = String.format("An error occurred when I tried to connect to `%s` subreddit.", value);
+            channel.sendMessage(message).queue();
+        }
+    }
+
+    private void removeSubreddit(CommandEvent event, String value) {
+        TextChannel channel = event.getChannel();
+        long guildId = event.getGuildId();
+        GuildResourceEntityManager<SubredditDB> entityManager = databaseService.getSubredditService().get(guildId);
+        value = value.toLowerCase(Locale.ROOT).trim();
+        SubredditDB entity = new SubredditDB(value, guildId);
+        removeFromDatabase(entityManager, entity, channel, "subreddit");
+    }
+
+    private void customizeImgur(CommandEvent event, String action, String value) {
+        if (action.equals("add")) {
+            addImgurTag(event, value);
+        } else if (action.equals("remove")) {
+            removeImgurTag(event, value);
+        } else {
+            String message = String.format(
+                    "`%s` is incorrect action, type `<prefix>help settings` to see usage of this command.", action
+            );
+            event.getChannel().sendMessage(message).queue();
+        }
+    }
+
+    private void addImgurTag(CommandEvent event, String value) {
+        TextChannel channel = event.getChannel();
+
+        value = value.toLowerCase(Locale.ROOT).trim();
+        if (!defaultImgurTags.contains(value)) {
+            String message = String.format(
+                    "`%s` is not supported Imgur tag. You can only add tags from bot default list of Imgur tags. " +
+                            "To see this list type `<prefix>settings list`.", value
+            );
+            channel.sendMessage(message).queue();
+            return;
+        }
+
+        long guildId = event.getGuildId();
+        GuildResourceEntityManager<ImgurTagDB> entityManager = databaseService.getImgurTagService().get(guildId);
+        ImgurTagDB entity = new ImgurTagDB(value, guildId);
+        addToDatabase(entityManager, entity, channel, "Imgur tag");
+    }
+
+    private void removeImgurTag(CommandEvent event, String value) {
+        TextChannel channel = event.getChannel();
+        long guildId = event.getGuildId();
+        GuildResourceEntityManager<ImgurTagDB> entityManager = databaseService.getImgurTagService().get(guildId);
+        value = value.toLowerCase(Locale.ROOT).trim();
+        ImgurTagDB entity = new ImgurTagDB(value, guildId);
+        removeFromDatabase(entityManager, entity, channel, "Imgur tag");
+    }
+
+    private <T extends GuildResourceEntity> void addToDatabase(
+            GuildResourceEntityManager<T> entityManager,
+            T entity,
+            TextChannel channel,
+            String listName
+    ) {
+
+        if (entityManager.contains(entity.getName())) {
+            String message = String.format("Provided %1$s is already on the guild %1$s list.", listName);
+            channel.sendMessage(message).queue();
+            return;
+        }
+
+        int limit = entityManager.getLimit();
+        if (entityManager.size() >= limit) {
+            String message = String.format("Guild can't have more than %d %s.", limit, listName + "s");
+            channel.sendMessage(message).queue();
+            return;
+        }
+
+        if (entityManager.add(entity)) {
+            String message = String.format("`%s` added to guild %s list.", entity.getName(), listName);
+            channel.sendMessage(message).queue();
+        } else {
+            String message = String.format("Failed to add `%s` to guild %s list.", entity.getName(), listName);
+            channel.sendMessage(message).queue();
+        }
+    }
+
+    private <T extends GuildResourceEntity> void removeFromDatabase(
+            GuildResourceEntityManager<T> entityManager,
+            T entity,
+            TextChannel channel,
+            String listName
+    ) {
+
+        if (!entityManager.contains(entity.getName())) {
+            String message = String.format("I couldn't find `%s` %2$s on guild %2$s list.", entity.getName(), listName);
+            channel.sendMessage(message).queue();
+            return;
+        }
+
+        if (entityManager.remove(entity.getName())) {
+            String message = String.format("`%s` %2$s has been removed from guild %2$s list.", entity.getName(), listName);
+            channel.sendMessage(message).queue();
+        } else {
+            String message = String.format("Failed to remove `%s` from guild %s list.", entity.getName(), listName);
+            channel.sendMessage(message).queue();
         }
     }
 
